5fca5e0aa49e3035309a6f2ed8218e242baa6849
J. Lee <78328779+ee-juni@users.noreply.github.com>
2023-07-11 12:10:37 +0900

    Add files via upload
    includes functions to get CUDA device numbers(indices) list sorted in a fashion of 'most free' to 'least free' (in terms of GPU memory)

---
+++ cuda_memory.py
@@ -0,0 +1,29 @@
+import nvidia_smi
+import numpy as np
+
+def get_free():
+    '''
+    Return list of percentage of free memory of all available CUDA devices.
+    '''
+    nvidia_smi.nvmlInit()
+    freem = []
+
+    for i in range(nvidia_smi.nvmlDeviceGetCount()):
+        info = nvidia_smi.nvmlDeviceGetMemoryInfo(nvidia_smi.nvmlDeviceGetHandleByIndex(i))
+        freem.append(100*info.free/info.total)
+    nvidia_smi.nvmlShutdown()
+
+    return freem
+
+def get_sorted():
+    '''
+    Return list of CUDA device numbers sorted from highest to lowest percentage of free memory
+    '''
+    freem = get_free()
+    l = []
+    for i in range(len(freem)):
+        max_idx = np.argmax(freem).item()
+        l.append(max_idx)
+        freem[max_idx]=-1
+    return l
+
